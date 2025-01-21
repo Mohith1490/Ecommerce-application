5417a62ec0f436b9a245ab46cb0c4d72a6a69ee1
Mohith1490 <mohith1754singh@gmail.com>
2025-01-21 18:08:00 +0530

    removed .env

--- frontend/src/components/ItemCardComponent.tsx
+++ frontend/src/components/ItemCardComponent.tsx
@@ -1,10 +1,10 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import '../global.css'
-import { addToCart } from '../redux/slice/cartSlice.ts'
-import { useAppDispatch } from '../redux/hooks.ts'
-import {Link} from 'react-router-dom'
+import { addToCart, deleteItem } from '../redux/slice/cartSlice.ts'
+import { useAppDispatch, useAppSelector } from '../redux/hooks.ts'
+import { Link } from 'react-router-dom'
 
- type cartDisplayLayout = {
+type cartDisplayLayout = {
   id: number,
   title: string,
   price: number,
@@ -13,14 +13,20 @@ import {Link} from 'react-router-dom'
 }
 
 
-const ItemCardComponent = ({id, title, price, description, image }: cartDisplayLayout) => {
+const ItemCardComponent = ({ id, title, price, description, image }: cartDisplayLayout) => {
 
   const dispatch = useAppDispatch();
-  const [clicked,setClicked] = useState(false);
-  function addToCartfunction(){
-    dispatch(addToCart({id, title, price, description, image,quantity:1}))
+  const cartItems = useAppSelector((state) => state.cart.items);
+  const isInCart = cartItems.some((item) => item.id === id);
+  const [clicked, setClicked] = useState(false);
+  function addToCartfunction() {
+    dispatch(addToCart({ id, title, price, description, image, quantity: 1 }))
     setClicked(!clicked)
-}
+  }
+  function RemoveItem() {
+    dispatch(deleteItem(id))
+    setClicked(!clicked)
+  }
   return (
     <>
       <div className='card' key={id} >
@@ -30,9 +36,13 @@ const ItemCardComponent = ({id, title, price, description, image }: cartDisplayL
           <p className='w-[14rem] h-7  overflow-y-hidden font-medium' >{description}</p>
           <span className='font-bold text-2xl ' >${price.toFixed(2)}<sub className='font-bold text-sm' ><s>${(price * 1.5).toFixed(2)}</s></sub> </span>
         </Link>
-        <button onClick={addToCartfunction}  className='w-[14rem] h-10 mb-3 border-2 bg-blue-600 hover:bg-blue-500 rounded-xl font-medium z-10'>
-            {clicked ? "Remove from cart" : "Add to cart"}
-        </button>
+        {isInCart ? <button onClick={RemoveItem} className='w-[14rem] h-10 mb-3 border-2 bg-blue-600 hover:bg-blue-500 rounded-xl font-medium z-10'>
+          Remove to cart
+        </button> :
+          <button onClick={addToCartfunction} className='w-[14rem] h-10 mb-3 border-2 bg-blue-600 hover:bg-blue-500 rounded-xl font-medium z-10'>
+            Add from cart
+          </button>
+        }
       </div>
     </>
   )
